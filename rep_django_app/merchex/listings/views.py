3ba1c14d45bd081dc1ecdfbf64b4dc6d0e483e76
Annie1911 <sessoustephanie5@gmail.com>
2024-03-20 15:47:30 +0000

    creation of model Band and Listings and hello,listings,base htmlpage

--- rep_django_app/merchex/listings/views.py
+++ rep_django_app/merchex/listings/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.models import Band,Listings
+
 
 
 
 def hello(request):
-    return HttpResponse('<h1>HELLO WORD </h1>')
+    bands = Band.objects.all()
+    return render (request,'listing/hello.html', {'bands': bands})
+
 
 
 def about(request):
     return HttpResponse('<h1>About us </h1> <p>nous aimons vous rendre service</p')
 
 def listings(request):
-    return HttpResponse('<ul><li>articles 1</li><li>articles 2</li><li>articles 3</li></ul>')
+    titles = Listings.objects.all()
+    return render(request, 'listing/listings.html',{'titres ': titles})
+
 
 def contact_us(request):
     return HttpResponse('<p>contacter nous </p>')
