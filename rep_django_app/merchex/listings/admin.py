31a411d5a189a7237fd73ff707dd7bd8599be253
Annie1911 <sessoustephanie5@gmail.com>
2024-03-21 13:53:08 +0000

    useful  CRUD

--- rep_django_app/merchex/listings/admin.py
+++ rep_django_app/merchex/listings/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from django.listings.models import Band
+
+admin.sit.register(Band)
+
 # Register your models here.
--- rep_django_app/merchex/listings/models.py
+++ rep_django_app/merchex/listings/models.py
@@ -1,9 +1,39 @@
 from django.db import models
+from django.core.validators import MinValueValidator,MaxValueValidator
 
+
+# listings/models.py
 
 class Band(models.Model):
+
+
+    class Genre(models.TextChoices):
+        HIP_HOP = 'HH'
+        SYNTH_POP = 'SP'
+        ALTERNATIVE_ROCK = 'AR'
+
     name = models.fields.CharField(max_length=100)
+    genre = models.fields.CharField(choices =Genre.choices, max_length=50)
+    biography = models.fields.CharField(max_length=1000)
+    year_formed = models.fields.IntegerField(
+    validators=[MinValueValidator(1900), MaxValueValidator(2021)]
+    )
+    active = models.fields.BooleanField(default=True)
+    official_homepage = models.fields.URLField(null=True, blank=True)
+
+
 
 class Listings(models.Model):
-    title = models.fields.CharField(max_length=100)
 
+    class Type(models.TextChoices):
+        Records ='Rd'
+        Clothing ='Cl' 
+        Posters ='Post'
+        Miscellaneous ='M'
+
+        
+    title = models.fields.CharField(max_length=100)
+    describe = models.fields.CharField(max_length=1000)
+    sold = models.fields.BooleanField(default=True)
+    year = models.fields.IntegerField(validators=[MinValueValidator(2001),MaxValueValidator(2024)])
+    type = models.fields.CharField( choices =Type.choices ,max_length=50)
